1e436ca7c0a5238a89034310767d11b96e893da9
ghost-lolade <horlorlerdey10@gmail.com>
2021-03-25 13:03:43 +0100

    edited Imagecontroller and route

--- app/Http/Controllers/ImagesController.php
+++ app/Http/Controllers/ImagesController.php
@@ -14,7 +14,7 @@ class ImagesController extends Controller
      */
     public function index()
     {
-        //
+        Models::select
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('test');
 });
 Route::get('/about', function () {
     return view('about');
